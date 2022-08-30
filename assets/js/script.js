76dcca7831572979be9ceb248b4b22487c53fead
Magalie Chetrit <email@magaliechetrit.com>
2022-08-30 16:20:08 +0300

    Update the sort & filter script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,57 @@
-/*const threads = [...document.querySelectorAll('.project-line')]
-const filters = {
-    searchText: ''
-}
+// 1st three lines position the filters to the right
+const filterList = document.querySelector('.js-filters'); 
+const lastMenuItem = document.querySelector('.main-menu').lastElementChild;
+lastMenuItem.append(filterList);
 
-const renderThreads = function (threads, filters) {
-    const filteredThreads = threads.filter(function (thread) {
-        return thread.textContent.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
+const sortKeywords = [];
 
-    const nonfilteredThreads = threads.filter(function (thread) {
-        return !thread.textContent.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
+document.querySelectorAll('.filter__link').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const filter = e.currentTarget;
+        filter.classList.toggle('active');
 
-     //console.log(nonfilteredThreads);
-     document.querySelector('.projects').innerHTML = ''
-    
-     const threadDiv = document.createElement('div')
-     threadDiv.classList.add('project');
-
-    filteredThreads.forEach(function (thread) {  
-        // document.querySelector('.projects').appendChild(threadDiv)
-        // document.querySelector('.project').appendChild(thread)
-        document.querySelector('.projects').appendChild(thread)
-    })
-
-    nonfilteredThreads.forEach(function (thread) {  
-        document.querySelector('.leftover-projects').appendChild(thread)
-    })
-
-    
-}   
-
-
-// renderThreads(threads, filters)
-const searchField = document.querySelector('.js-search-field')
-if (searchField) {
-    searchField.addEventListener('input', function(e) {
-        filters.searchText = e.target.value
-        setTimeout(renderThreads(threads, filters), 400)
-    })
-}
-
-// fetch inline-image by part of it's classname
-// add a new classname so user doesn't have to do it.
-const inlineImages = document.querySelectorAll("img[class^='wp-image-']")
-inlineImages.forEach(function (inlineImage){
-    inlineImage.classList.add('inline-image')
-}) 
-
-// Move filterlist to main menu because we don't know a different solution right now
-const filterList = document.querySelector('.js-filters')
-const lastMenuItem = document.querySelector('.main-menu').lastElementChild
-lastMenuItem.append(filterList)
-
-const filterLink = document.querySelectorAll('.filter__link')
-const tag = filterLink.textContent
-
-for (let i = 0; i < filterList.length; i++) {
-    filterLink[i].addEventListener('click', function(e) {
-        e.stopImmediatePropagation();
-         const filteredThreads = threads.filter(function (thread) {
-            return thread.textContent.toLowerCase().includes(tag.toLowerCase())
-        })
-         document.querySelector('.projects').innerHTML = ''
+        const filterText = filter.textContent.toLowerCase();
+        if(sortKeywords.includes(filterText)) {
+            sortKeywords.splice(sortKeywords.indexOf(filterText), 1); 
+        }
+        else {
+            sortKeywords.push(filterText); 
+        }
         
-        filteredThreads.forEach(function (thread) {
-            const threadDiv = document.createElement('div')
-            threadDiv.classList.add('project');
-            document.querySelector('.projects').appendChild(threadDiv)
-            document.querySelector('.project').appendChild(thread)
-        })
-    })
-}
+        document.querySelectorAll(`.projects .project-line a`).forEach(a => {
+            const project = a.closest('.project-line');
+            project.classList.add('hide');
+            project.classList.remove('sort'); 
 
-*/
+            sortKeywords.forEach(sort => {
+                if(a.getAttribute('data-tag').trim().split(/\s+/).includes(sort)) {
+                    project.classList.remove('hide'); 
+                    project.classList.add('sort');
+                } 
+            });
+            
+            
+        });
+
+        if(!sortKeywords.length) document.querySelectorAll(`.projects .project-line`).forEach(a => a.classList.remove('hide'));
+    });
+});
+
+
+document.querySelector('.search-field.js-search-field').addEventListener('input', e => {
+    const userInput = e.currentTarget.value.trim().toLowerCase(); 
+
+    const sortedLinks = document.querySelector('.filter__link.active');
+    document.querySelectorAll(`.projects .project-line${sortedLinks ? '.sort' : ''}`).forEach(project => {
+        const text = project.textContent.toLowerCase();
+        if(!text.includes(userInput)) {
+            project.classList.add('hide');
+        }
+        else {
+            project.classList.remove('hide');
+        }
+    });
+    
+});
+
