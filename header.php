7e450817057fd2c38e7bd333bec4d34dc08741e9
Magalie Chetrit <email@magaliechetrit.com>
2022-08-30 16:39:42 +0300

    added the filter buttons active state styling to header

--- header.php
+++ header.php
@@ -20,7 +20,12 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"> -->
      <!-- <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
     <?php wp_head(); ?>
-
+    <style>
+        .filter__link.active {
+            font-weight: bold;
+            font-style: italic;
+        }
+    </style>
 </head>
 
 <body <?php body_class(); ?>>
