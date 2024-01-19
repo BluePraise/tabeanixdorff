c3fda15683d7c893843906358e3c52ff32707897
Magalie Chetrit <email@magaliechetrit.com>
2024-01-18 20:08:56 -0500

    responsive styling for menu

--- template-parts/filters.php
+++ template-parts/filters.php
@@ -1,11 +1,12 @@
 
 <ul class="js-filters sub-menu">
-	<li class="clear-button"><a class="hide-this clear-active" href="javascript:clear_all()" title="clear all filters"><?php echo esc_html( "clear all filters" ); ?></a></li>
+	<li class="clear-button"><a class="hide-this clear-active" href="javascript:clear_all()" title="clear all filters"><?php echo "clear all filters"; ?></a></li>
 	    <?php
 	    $tags = get_tags();
 	    if ( $tags ) :
-	        foreach ( $tags as $tag ) : ?>
-	            <li><a class="filter__link" href="<?php echo esc_url( get_tag_link( $tag->term_id ) ); ?>" title="<?php echo esc_attr( $tag->name ); ?>"><?php echo esc_html( $tag->name ); ?></a></li>
+	        foreach ( $tags as $tag ) :
+			?>
+	            <li><a class="filter__link <?php if ( $tag->count == 0 ) : echo 'inactive'; endif; ?>" href="<?php echo esc_url( get_tag_link( $tag->term_id ) ); ?>" title="<?php echo esc_attr( $tag->name ); ?>"><?php echo esc_html( $tag->name ); ?></a></li>
 	        <?php endforeach; ?>
 	    <?php endif; ?>
 </ul>
